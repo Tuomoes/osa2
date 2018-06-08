0b5706c35ce3c153da5a77cf8af70e43a0f963fe
Tuomoes <tuomo_eskola@hotmail.com>
2018-06-08 11:41:28 +0300

    subtask 2.4 done

--- kokoelmien_renderointi/src/index.js
+++ kokoelmien_renderointi/src/index.js
@@ -1,5 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
+const Kurssit = (props) => {
+    console.log("kurssit", props.kurssit)
+    return (
+        <div>
+            {props.kurssit.map(kurssi=><Kurssi key={kurssi.id} kurssi={kurssi}/>)}
+        </div>
+    )
+}
+
+const Kurssi = ({kurssi}) => {
+    console.log("kurssi", kurssi)
+    console.log("osat", kurssi.osat)
+    return (
+        <div>
+            <Otsikko kurssi={kurssi}/>
+            <Sisalto osat={kurssi.osat}/>
+            <Yhteensa osat={kurssi.osat}/>
+        </div>
+    )
+}
+
 const Otsikko = (props) => {
     return (
       <div>
@@ -8,17 +31,8 @@ const Otsikko = (props) => {
     )
   }
 
-const Kurssi = (props) => {
-    return (
-        <div>
-            <Otsikko kurssi={props.kurssi}/>
-            <Sisalto osat={props.kurssi.osat}/>
-            <Yhteensa osat={props.kurssi.osat}/>
-        </div>
-    )
-}
-
 const Sisalto = (props) => {
+    console.log("osat", props.osat)
     return (
         <div>
             {props.osat.map(osa=><Osa key={osa.id} nimi={osa.nimi} tehtavia={osa.tehtavia}/>)}
@@ -47,35 +61,49 @@ const Yhteensa = (props) => {
 
 
   const App = () => {
-    const kurssi = {
-      nimi: 'Half Stack -sovelluskehitys',
-      osat: [
+    const kurssit = [
         {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
+          nimi: 'Half Stack -sovelluskehitys',
+          id: 1,
+          osat: [
+            {
+              nimi: 'Reactin perusteet',
+              tehtavia: 10,
+              id: 1
+            },
+            {
+              nimi: 'Tiedonvälitys propseilla',
+              tehtavia: 7,
+              id: 2
+            },
+            {
+              nimi: 'Komponenttien tila',
+              tehtavia: 14,
+              id: 3
+            }
+          ]
         },
         {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        },
-        {
-            nimi: 'Redux',
-            tehtavia: 7,
-            id: 4
+          nimi: 'Node.js',
+          id: 2,
+          osat: [
+            {
+              nimi: 'Routing',
+              tehtavia: 3,
+              id: 1
+            },
+            {
+              nimi: 'Middlewaret',
+              tehtavia: 7,
+              id: 2
+            }
+          ]
         }
       ]
-    }
   
     return (
       <div>
-        <Kurssi kurssi={kurssi} />
+        <Kurssit kurssit={kurssit} />
       </div>
     )
   }
