4542bd625cce44396fe12e72a904fe1ecd0ce4de
Tuomoes <tuomo_eskola@hotmail.com>
2018-06-13 11:29:34 +0300

    subtask 2.6 name list render correctly. Adding new lines not implemented yet.

--- lomakkeet/src/App.js
+++ lomakkeet/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+const Persons = (props) => {
+    return (
+        <div>
+            <table>
+                <tbody> 
+                    {props.persons.map(person => <Person key={person.name} name={person.name}/>)}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+const Person = ({name}) => {
+    return (
+        <tr>
+            <td>{name}</td>
+        </tr>
+    )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas' },
+        { name: 'Joku Toinen'}
       ],
       newName: ''
     }
@@ -25,6 +46,8 @@ class App extends React.Component {
         </form>
         <h2>Numerot</h2>
         
+        <Persons persons={this.state.persons} />
+
       </div>
     )
   }
