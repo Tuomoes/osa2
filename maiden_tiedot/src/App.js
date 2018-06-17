896cae1de014066b2a7e4f7078a28db319e0f8cc
Tuomoes <tuomo_eskola@hotmail.com>
2018-06-17 21:14:44 +0300

    subtask 2.12* can now show countries from the api

--- maiden_tiedot/src/App.js
+++ maiden_tiedot/src/App.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const Countries = (props) => {
+  return (
+    <div>
+      <table>
+       <tbody>
+        {props.countries.map((country) => <Country key={country.name} name={country.name}/>)}
+       </tbody>
+      </table>
+   </div>
+  )
+
+}
+
+const Country = ({name}) => {
+  return (
+    <tr>
+      <td>{name}</td>
+    </tr>
+  )
+
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      countries: [
+        {name: ''}
+      ]
+    }
+  }
+
+  componentDidMount() {
+    console.log('mounted!')
+    axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+      console.log(response)
+      this.setState({countries: response.data})
+    })
+  }
+
   render() {
     return (
       <div>
-        moi
+        <Countries countries={this.state.countries}/>
       </div>
     );
   }
